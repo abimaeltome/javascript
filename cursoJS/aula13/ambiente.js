bed47865a7d7bf74e04e4d3c643edc5abcaca37b
abimaeltome <142822485+abimaeltome@users.noreply.github.com>
2023-10-21 16:46:05 -0300

    casa

---
+++ cursoJS/aula13/ambiente.js
@@ -0,0 +1,29 @@
+var c = 1
+
+do {
+    console.log (`Tudo bem? passo ${c}`)
+    c++
+
+} while (c <= 6)
+
+
+/*
+
+var c = 1
+while (c <= 6){
+    console.log (`Tudo bem? passo ${c}`)
+    c++
+}
+
+*/
+
+/*
+
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/
