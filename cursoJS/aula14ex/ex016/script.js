a0c33f4836b7689c7a679721b2a80f74a4e9d99c
abimaeltome <142822485+abimaeltome@users.noreply.github.com>
2023-10-22 13:25:46 -0300

    ok

--- cursoJS/aula14ex/ex016/script.js
+++ cursoJS/aula14ex/ex016/script.js
@@ -1,1 +1,16 @@
-alert('Olá!')
+function contar() {
+    var inicio = window.document.querySelector('input#txtini')
+    var fim = window.document.querySelector('input#txtfim')
+    var passo = document.querySelector('input#txtpas')
+    var resultado = document.querySelector('div#res')
+    do {
+        resultado = inicio += passo
+        res.innerHtml = `Contando:<br> ${resultado}` 
+    } while (resultado < fim);
+
+    
+}
+
+
+
+
