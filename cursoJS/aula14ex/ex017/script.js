56bc6aa790e3124f9ee40a3e2ee12c8d49a45237
abimaeltome <142822485+abimaeltome@users.noreply.github.com>
2023-10-22 21:12:12 -0300

    casa

--- cursoJS/aula14ex/ex017/script.js
+++ cursoJS/aula14ex/ex017/script.js
@@ -1,1 +1,22 @@
-alert('Olá!')
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')
+        
+    } else {
+        
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ""
+        while (c <= 10) {
+            let item = document.createElement('option') // CRIA O option DENTRO DO HTML
+            item.text = `${n} x ${c} = ${n*c}`// CONTEÚDO DO option
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+            
+        }
+        
+    }
+}
